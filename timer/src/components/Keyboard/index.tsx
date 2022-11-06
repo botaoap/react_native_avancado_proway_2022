b3cd567a9c850685efe513cbb22a000a749f7dbf
lordefps <gabrielbotaoap@gmail.com>
2022-11-06 19:03:46 -0300

    component keyboard

--- timer/src/components/Keyboard/index.tsx
+++ timer/src/components/Keyboard/index.tsx
@@ -1,7 +1,54 @@
 import { View } from "react-native";
+import { useTimer } from "../../contexts/TimerContext";
+import { Button } from "../Button";
+import { styles } from "./styles";
 
 export function Keyboard() {
+    const { timer, setTimer } = useTimer();
+
+    function onKeyPress(key: string) {
+        if (timer.length >= 6) {
+            return;
+        }
+
+        setTimer(prev => prev + key)
+    }
+
+    function onBackPress() {
+        if (timer.length) {
+            setTimer(prev => prev.slice(0,-1))
+        }
+    }
+
     return (
-        <View></View>
+        <>
+            <View style={styles.rowsContainer}>
+                <View style={styles.row}>
+                    <Button text="1" onPress={() => onKeyPress("1")} />
+                    <Button text="2" onPress={() => onKeyPress("2")} />
+                    <Button text="3" onPress={() => onKeyPress("3")} />
+                </View>
+                <View style={styles.row}>
+                    <Button text="4" onPress={() => onKeyPress("4")} />
+                    <Button text="5" onPress={() => onKeyPress("5")} />
+                    <Button text="6" onPress={() => onKeyPress("6")} />
+                </View>
+                <View style={styles.row}>
+                    <Button text="7" onPress={() => onKeyPress("7")} />
+                    <Button text="8" onPress={() => onKeyPress("8")} />
+                    <Button text="9" onPress={() => onKeyPress("9")} />
+                </View>
+                <View style={styles.row}>
+                    <Button text="00" onPress={() => onKeyPress("00")} />
+                    <Button text="0" onPress={() => onKeyPress("0")} />
+                    <Button text="⌫" color="grey" onPress={() => onBackPress()}/>
+                </View>
+            </View>
+            <View style={styles.buttonPlayContainer}>
+                {
+                    !!timer.length && <Button text="▶" color="blue" onPress={() => {}}/>
+                }
+            </View>
+        </>
     )
 }
---
+++ timer/src/components/Keyboard/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    rowsContainer: {
+        alignContent: "center",
+        margin: 10
+    },
+    row: {
+        flexDirection: "row"
+    },
+    buttonPlayContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        height: 100   
+    }
+});
