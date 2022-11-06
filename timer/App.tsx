9064f20cbb29fc73af3e474c68b87a403da93aed
lordefps <gabrielbotaoap@gmail.com>
2022-11-06 15:55:36 -0300

    styles to button

--- timer/App.tsx
+++ timer/App.tsx
@@ -5,7 +5,11 @@ import { Button } from './src/components/Button';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Button text='0' />
+      <Button text="0" />
+      <Button text="1" color='blue'/>
+      <Button text="2" color='purple'/>
+      <Button text="3" color='grey'/>
+      <Button text="4" color='grey' size='small'/>
       <StatusBar style="auto" />
     </View>
   );
--- timer/src/components/Button/index.tsx
+++ timer/src/components/Button/index.tsx
@@ -1,21 +1,35 @@
 import { Text, TouchableOpacity } from "react-native";
+import { colors } from "../../styles/color";
 import { styles } from "./styles";
 
 type Props = {
     text: string;
-    color:"blue" | "grey" | "purple";
-    size: "small" |  "normal";
+    color?:"blue" | "grey" | "purple";
+    size?: "small" |  "normal";
 }
 
 export function Button(props: Props) {
-    let color = "";
+    let backgroundColor = "";
+    let size = { height: 92, width: 92 };
+
+    if (props.size == "small") {
+        size = { height: 46, width: 46 };
+    }
 
     switch (props.color) {
         case "blue":
-
+            backgroundColor = colors.buttonBlue;
+            break;
+        case "grey":
+            backgroundColor = colors.buttonGrey;
+            break;
+        case "purple":
+            backgroundColor = colors.buttonPurple;
+            break;
     }
+
     return (
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={[styles.button, { backgroundColor, ...size }]}>
             <Text style={styles.text}> {props.text} </Text>
         </TouchableOpacity>
     )
