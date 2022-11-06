890c8638d5b578960e895aaa2a91e1902b75aeee
lordefps <gabrielbotaoap@gmail.com>
2022-11-06 19:04:42 -0300

    timer definition screen

---
+++ timer/src/screens/TimerDefinitionScreen/index.tsx
@@ -0,0 +1,11 @@
+import { Keyboard } from "../../components/Keyboard";
+import { Display } from "../Display";
+
+export function TimerDefinition() {
+    return (
+        <>
+            <Display />
+            <Keyboard/>
+        </>
+    );
+}
