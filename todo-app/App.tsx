1a1762b7794bded907c388574b1994194f560105
botaoap <gabrielbotaoap@gmail.com>
2022-12-11 16:31:50 -0300

    change navigations in app

--- todo-app/App.tsx
+++ todo-app/App.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { SafeAreaView, StatusBar, Text } from 'react-native';
-import { SignInScreen } from './src/screens/SignInScreen';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { theme } from './src/themes';
-import { HomeScreen } from './src/screens/HomeScreen';
 import { TodoProvider } from './src/context/TodoContext';
+import { NavigationContainer } from '@react-navigation/native';
+import { AuthProvider } from './src/context/AuthContext';
+import { AppRoutes } from './src/routes/AppRoutes';
 
 export default function App() {
   return (
-    <PaperProvider theme={theme}>
-      <SafeAreaView style={{ flex: 1 }}>
-        <StatusBar />
-        {/* <SignInScreen /> */}
-        <TodoProvider>
-          <HomeScreen />
-        </TodoProvider>
-      </SafeAreaView>
-    </PaperProvider>
+    <NavigationContainer>
+      <PaperProvider theme={theme}>
+        <SafeAreaView style={{ flex: 1 }}>
+          <StatusBar />
+          <AuthProvider>
+            <TodoProvider>
+              <AppRoutes />
+            </TodoProvider>
+          </AuthProvider>
+        </SafeAreaView>
+      </PaperProvider>
+    </NavigationContainer>
   );
 }
